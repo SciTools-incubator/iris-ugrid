83184e324e7872e66a7f8b2350dab248456cef07
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2020-08-06 12:38:27 +0100

    ÂTravis including iris installation, with one temporary test.

---
+++ iris_ugrid/tests/__init__.py
@@ -0,0 +1,6 @@
+# Copyright Iris-ugrid contributors
+#
+# This file is part of Iris and is released under the LGPL license.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
+"""All tests for the :mod:`iris-ugrid` package."""
