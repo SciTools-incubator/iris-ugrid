a8f29647c3af711da85d7b01933a863a8dbaefa8
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2020-08-06 16:54:31 +0100

    Pytest style, with failing test to check Travis.

--- iris_ugrid/tests/test_install.py
+++ iris_ugrid/tests/test_install.py
@@ -9,18 +9,15 @@ Test that the correct iris is installed, including iris-ugrid support.
 TODO: remove this when we have other more sensible tests.
 
 """
-# import iris tests first, to initialise before importing anything else
-import iris.tests as tests
+
+def test_iris_installation():
+    # Check that iris cf loader includes 'exclude' functionality.
+    # Import a ugrid-specific test, that ought to exist on the branch.
+    from iris.tests.unit.fileformats.cf.test_CFReader \
+        import Test_exclude_vars
+
+    assert hasattr(Test_exclude_vars, 'test_exclude_vars')
 
 
-class Test(tests.IrisTest):
-    def test_iris_installation(self):
-        # Check that iris cf loader includes 'exclude' functionality.
-        # Import a ugrid-specific test, that ought to exist on the branch.
-        from iris.tests.unit.fileformats.cf.test_CFReader \
-            import Test_exclude_vars
-        self.assertTrue(hasattr(Test_exclude_vars, 'test_exclude_vars'))
-
-
-if __name__ == "__main__":
-    tests.main()
+def test_failure():
+    assert False
