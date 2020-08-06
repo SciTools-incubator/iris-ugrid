ac4f1309ac55d6aef3169c731684f79766f5bcbe
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2020-08-06 15:12:28 +0100

    Add travis testing (#11)
    * Add main + test folders.
    
    * ÂTravis including iris installation, with one temporary test.
    
    * Try to fix travis syntax.
    
    * Include stickler; fix python=3.7.
    
    * Add flake8 config.
    
    * Correct syntax + flake8 warnings.
    
    * Add main __init__; debug travis test script section.
    
    * More tweaks.
    
    * Revert to setup.py install for iris.
    
    * Try to fix cwd in travis.
    
    * Tidy travis config.
    
    * Further Travis tidying; reduce flake8 exceptions; use conda requirements file.
    
    * Pep8 fix.

---
+++ iris_ugrid/tests/test_install.py
@@ -0,0 +1,26 @@
+# Copyright Iris-ugrid contributors
+#
+# This file is part of Iris and is released under the LGPL license.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
+"""
+Test that the correct iris is installed, including iris-ugrid support.
+
+TODO: remove this when we have other more sensible tests.
+
+"""
+# import iris tests first, to initialise before importing anything else
+import iris.tests as tests
+
+
+class Test(tests.IrisTest):
+    def test_iris_installation(self):
+        # Check that iris cf loader includes 'exclude' functionality.
+        # Import a ugrid-specific test, that ought to exist on the branch.
+        from iris.tests.unit.fileformats.cf.test_CFReader \
+            import Test_exclude_vars
+        self.assertTrue(hasattr(Test_exclude_vars, 'test_exclude_vars'))
+
+
+if __name__ == "__main__":
+    tests.main()
