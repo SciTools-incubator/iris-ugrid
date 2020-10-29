fe514e3a50f8bae130b6f2db8d59a4377b620c4b
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2020-10-29 11:12:07 +0000

    Add test for ucube._repr_html_.

--- iris_ugrid/tests/unit/ucube/test_UCube.py
+++ iris_ugrid/tests/unit/ucube/test_UCube.py
@@ -6,8 +6,9 @@
 """
 Test basic :class:`iris_ugrid.ucube.Ucube` object.
 """
-
 import iris.tests as tests
+
+import re
 
 from iris import Constraint
 from iris.tests import IrisTest, get_data_path
@@ -25,19 +26,25 @@ class Test_cube_representations(IrisTest):
         loaded_cubes = CubeList(load_cubes(file_path))
         (cube,) = loaded_cubes.extract(Constraint("theta"))
         # Prune the attributes, just because there are a lot.
-        keep_attrs = ['timeStamp', 'Conventions']
-        cube.attributes = {key: value
-                           for key, value in cube.attributes.items()
-                           if key in keep_attrs}
+        keep_attrs = ["timeStamp", "Conventions"]
+        cube.attributes = {
+            key: value
+            for key, value in cube.attributes.items()
+            if key in keep_attrs
+        }
         self.ucube = cube
 
-    def test_cube_summary_short(self):
+    def test_summary_short(self):
+        # Check the short-form of a UCube summary.
+        # This the same as what will appear in a CubeList string repr.
         result = self.ucube.summary(shorten=True)
-        expected = ('Potential Temperature / (K)         '
-                    '(time: 1; levels: 6; *-- : 866)')
+        expected = (
+            "Potential Temperature / (K)         "
+            "(time: 1; levels: 6; *-- : 866)"
+        )
         self.assertEqual(result, expected)
 
-    def test_cube_summary_long(self):
+    def test_summary_long(self):
         result = str(self.ucube)
         expected = """\
 Potential Temperature / (K)         (time: 1; levels: 6; *-- : 866)
@@ -58,6 +65,20 @@ Potential Temperature / (K)         (time: 1; levels: 6; *-- : 866)
           point: time\
 """
         self.assertEqual(result, expected)
+
+    def test__repr_html_(self):
+        result = self.ucube._repr_html_()
+        # Check for some key pieces of html, which indicate that it includes
+        # a summary of the unstructured dimension, and mesh details.
+        str_dim = '<th class="iris iris-word-cell">*--</th>'
+        self.assertIn(str_dim, result)
+        str_section = \
+            '<td class="iris-title iris-word-cell">Unstructured mesh</td>'
+        self.assertIn(str_section, result)
+        re_mesh = (r'<td class="iris-word-cell iris-subheading-cell">'
+                   r'\s*Mesh0\s*</td>'
+                   r'\s*<td class="iris-inclusion-cell">node</td>')
+        self.assertIsNotNone(re.search(re_mesh, result))
 
 
 if __name__ == "__main__":
